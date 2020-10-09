ee99d4504312a9213a5dd1ce1bb2752952e89049
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-10-08 22:14:16 -0500

    finished creating views section

---
+++ Completed SQL/updatable views.sql
@@ -0,0 +1,28 @@
+-- updatable views
+
+-- distinct
+-- aggregrate functions(min, max, sum)
+-- group by/ having
+-- union
+
+
+create or replace view invoices_with_balance as
+select 
+	invoice_id,
+    number,
+    client_id,
+    invoice_total,
+    payment_total,
+    invoice_total - payment_total as balance,
+    invoice_date,
+    due_date,
+    payment_date
+from invoices
+where (invoice_total - payment_total) > 0;
+
+delete from invoices_with_balance
+where invoice_id = 1;
+
+update invoices_with_balance
+set due_date = date_add(due_date, interval 2 day)
+where invoice_id = 2;
